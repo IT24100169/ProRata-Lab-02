6746f08dea7017054208acb49be7380ce4e290f2
IT24100169 <IT24100169@my.sliit.lk>
2026-09-18 19:52:07 +0530

    IT24100169

---
+++ Q1/Lab2Q1.java
@@ -0,0 +1,12 @@
+
+public class Lab2Q1 {
+    public static void main(String[] args) {
+        double perimeter = 100;
+        
+        double length = perimeter / 3.5;
+        double width  = 0.75 * length;
+
+        System.out.println("Length of the fence: " + length);
+        System.out.println("Width of the fence: " + width);
+    }
+}
